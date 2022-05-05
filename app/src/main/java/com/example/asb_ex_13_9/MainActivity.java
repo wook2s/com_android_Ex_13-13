dea792df5067a98fcfb7e4567be162eccdfc34ec
jade <jade0911k@gmail.com>
2022-05-05 18:50:00 +0900

    UI Thread

--- app/src/main/java/com/example/asb_ex_13_9/MainActivity.java
+++ app/src/main/java/com/example/asb_ex_13_9/MainActivity.java
@@ -7,8 +7,12 @@ import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    T1 t1;
+    boolean flagForThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +23,14 @@ public class MainActivity extends AppCompatActivity {
         ProgressBar pb2 = (ProgressBar) findViewById(R.id.sb2);
         Button btnStart = (Button) findViewById(R.id.btnStart);
 
+        TextView tv1 = (TextView) findViewById(R.id.tv1);
+        TextView tv2 = (TextView) findViewById(R.id.tv2);
+
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                T1 t1 = new T1(pb1);
+                flagForThread = true;
+                t1 = new T1(pb1, tv1, flagForThread);
                 t1.start();
 
                 new Thread(){
@@ -30,7 +38,17 @@ public class MainActivity extends AppCompatActivity {
                     public void run() {
                         super.run();
                         for(int i=0; i<100; i++){
-                            pb2.setProgress(pb2.getProgress() + 1);
+                            if(flagForThread == false){
+                                break;
+                            }
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    pb2.setProgress(pb2.getProgress() + 1);
+                                    tv2.setText("진행률 : "+pb2.getProgress());
+                                }
+                            });
+
                             SystemClock.sleep(100);
                         }
                     }
@@ -39,24 +57,60 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+        Button btnReset = (Button) findViewById(R.id.btnReset);
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                flagForThread = false;
+                t1.stopThread(flagForThread);
+
+                pb1.setProgress(10);
+                pb2.setProgress(30);
+            }
+        });
+
+
+
+        Button btnStop = (Button) findViewById(R.id.btnStop);
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                flagForThread = false;
+                t1.stopThread(flagForThread);
+            }
+        });
+
     }
+
 }
 
 class T1 extends Thread{
 
     ProgressBar pb;
+    TextView tv;
+    boolean flag;
 
-    T1(ProgressBar pb){
+    T1(ProgressBar pb, TextView tv, boolean flag){
         this.pb = pb;
+        this.tv = tv;
+        this.flag = flag;
     }
 
     @Override
     public void run() {
+        //flag = true;
         super.run();
         for(int i=0; i<100; i++){
+            if(flag == false){
+                break;
+            }
             pb.setProgress(pb.getProgress()+2);
+            tv.setText("진행률 : " + pb.getProgress());
             SystemClock.sleep(100);
         }
 
+    }
+    public void stopThread(boolean flag){
+        this.flag = flag;
     }
 }
